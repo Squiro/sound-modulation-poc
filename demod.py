c04a28fae87b09b9ab0370819b38f7bc58a7a2c9
Squiro <alesio_sinopoli@hotmail.com>
2025-05-20 15:17:24 -0300

    Replace mode with amp count

--- demod.py
+++ demod.py
@@ -2,6 +2,7 @@ from scipy.io import wavfile
 import numpy as np
 from scipy import stats
 from constants import FRAMES_PER_BEEP, PATTERN
+from utils import plot_signal
 
 AMPLITUDE_THRESHOLD = 200
 
@@ -10,14 +11,13 @@ AMPLITUDE_THRESHOLD = 200
 def find_peak(data):
     it = np.nditer(data, flags=["f_index"])
     for x in it:
-        if abs(x) > AMPLITUDE_THRESHOLD:
+        if abs(x) >= AMPLITUDE_THRESHOLD:
             print(f"Amplitude peak {x} at idx {it.index}")
             return it.index
 
 
 def demod(filename: str):
     sr, data = wavfile.read(filename)
-
     peak_index = find_peak(data)
     if peak_index == -1:
         print("No peak amplitude found")
@@ -25,6 +25,7 @@ def demod(filename: str):
 
     array_to_process = data[peak_index:]
     process_frames(array_to_process)
+    plot_signal(data)
 
 
 def process_frames(data):
@@ -36,10 +37,18 @@ def process_frames(data):
     end = fpr
     while end < arr_length:
         temp_arr = data[start:end]
-        m = stats.mode(temp_arr)
-        mode = abs(m[0][0])
-        print(mode)
-        bit_list.append("1" if mode > AMPLITUDE_THRESHOLD else "0")
+        np.abs(temp_arr, out=temp_arr)
+        # Count number of times amplitude is bigger than threshold
+        val = (temp_arr >= AMPLITUDE_THRESHOLD).sum()
+        # print(val)
+
+        # Previous experiments that used mean / mode
+        # m = stats.mode(temp_arr)
+        # val = abs(np.mean(temp_arr))
+        # print("mean: ", val)
+        # mode = abs(m[0][0])
+        # print("mode: ", mode)
+        bit_list.append("1" if val >= 200 else "0")
         start += fpr
         end += fpr
 
@@ -52,11 +61,20 @@ def decode_bits(bit_string: str):
     pattern_length = len(PATTERN)
     start = bit_string.find(PATTERN)
 
+    if start == -1:
+        print("Start pattern not found :(")
+        return
+
     # remove starting pattern
     bit_string = bit_string[start + pattern_length :]
     print(bit_string)
     # remove end pattern
     end = bit_string.find(PATTERN)
+
+    if end == -1:
+        print("End pattern not found :(")
+        return
+
     bit_string = bit_string[0:end]
     print(bit_string)
 
